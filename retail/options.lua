eb2bfdbdad6ca7bcbf6143e03a6ec7670e9933ce
ticstyle <4126225+ticstyle@users.noreply.github.com>
2024-08-05 14:20:20 +0200

    Update options.lua

--- retail/options.lua
+++ retail/options.lua
@@ -1,31 +1,30 @@
-local L = LibStub("AceLocale-3.0"):GetLocale("StarCursor", false)
+-- Slash command to open color picker
+SLASH_COLORPICKER1 = "/starcolour"
+SlashCmdList["COLORPICKER"] = function()
+    local prevr, prevg, prevb, preva = StarCursor.texture:GetVertexColor()
+    StarCursor.texture:Show();
+    ColorPickerFrame:SetupColorPickerAndShow({
+        r = StarCursorSettings.r,
+        g = StarCursorSettings.g,
+        b = StarCursorSettings.b,
+        opacity = StarCursorSettings.alpha,
+        hasOpacity = true,
+        swatchFunc = function()
+            local r, g, b = ColorPickerFrame:GetColorRGB()
+            local a = ColorPickerFrame:GetColorAlpha()
+            StarCursor.texture:SetVertexColor(r, g, b, a)
+            StarCursorSettings.r, StarCursorSettings.g, StarCursorSettings.b, StarCursorSettings.alpha = r, g, b, a
+        end,
+        cancelFunc = function()
+            StarCursor.texture:SetVertexColor(prevr, prevg, prevb, preva)
+            StarCursorSettings.r, StarCursorSettings.g, StarCursorSettings.b, StarCursorSettings.alpha = prevr, prevg, prevb, preva
+        end
+    })
+end
 
-hooksecurefunc("InterfaceOptionsFrame_OpenToCategory",InterfaceOptionsFrame_OpenToCategory)
-
--- Blizzard options frame
-        local panel = CreateFrame("Frame", "StarCursorBlizzOptions")
-        panel.name = "StarCursor"
-        InterfaceOptions_AddCategory(panel)
- 
-
-        local fs = panel:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
-        fs:SetPoint("TOPLEFT", 10, -15)
-        fs:SetPoint("BOTTOMRIGHT", panel, "TOPRIGHT", 10, -45)
-        fs:SetJustifyH("LEFT")
-        fs:SetJustifyV("TOP")
-        fs:SetText("StarCursor")
-
-        local button = CreateFrame("Button", nil, panel, "UIPanelButtonTemplate")
-        button:SetText(L['Configure'])
-        button:SetWidth(128)
-        button:SetPoint("TOPLEFT", 10, -48)
-        button:SetScript('OnClick', function()
-            
-            print("StarCursor: Options are planned for future updates. ")
-        end)
-
-		-- Slash Handler
-		SLASH_STARCURSOR1 = "/sc"
-		SlashCmdList.STARCURSOR = function(msg)
-	InterfaceOptionsFrame_OpenToCategory(StarCursorBlizzOptions)
+-- Slash command for help
+SLASH_HELP1 = "/starcursor"
+SlashCmdList["HELP"] = function()
+    print("StarCursor:")
+    print("/starcolour - Opens a color picker")
 end
