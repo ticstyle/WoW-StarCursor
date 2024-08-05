d331a5ad29be27711b201780c523cc81e85eff9a
ticstyle <4126225+ticstyle@users.noreply.github.com>
2024-08-05 14:19:38 +0200

    Update core.lua

--- retail/core.lua
+++ retail/core.lua
@@ -1,43 +1,79 @@
+StarCursor = StarCursor or {}
+
+-- Create StarCursor
 local frame = CreateFrame("Frame", nil, UIParent);
 frame:SetFrameStrata("TOOLTIP");
-local texture = frame:CreateTexture();
-texture:SetTexture([[Interface\Cooldown\star4]]);
---texture:SetVertexColor(1, 0, 0,.5);
-texture:SetBlendMode("ADD");
-texture:SetAlpha(0.5);
-local x = 0;
-local y = 0;
-local speed = 0;
+StarCursor.texture = frame:CreateTexture();
+StarCursor.texture:SetTexture([[Interface\Cooldown\star4]]);
+StarCursor.texture:SetBlendMode("ADD");
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("PLAYER_LOGOUT")
+
+
+frame:SetScript("OnEvent", function(self, event, arg1)
+  if event == "ADDON_LOADED" and arg1 == "StarCursor" then
+    -- Print slash commands
+    print("/starcursor - for options")
+    -- Our saved variables, if they exist, have been loaded at this point.
+    if StarCursorSettings then
+        -- Apply the saved settings
+        StarCursor.texture:SetVertexColor(StarCursorSettings.r, StarCursorSettings.g, StarCursorSettings.b, StarCursorSettings.alpha or 0.5);
+    else
+        -- This is the first time this addon is loaded; set SVs to default values
+        StarCursorSettings = {
+            r = 1,
+            g = 1,
+            b = 1,
+            alpha = 0.5
+        }
+        StarCursor.texture:SetVertexColor(1, 1, 1, 0.5);
+    end
+  elseif event == "PLAYER_LOGOUT" then
+      -- Save the current settings before logging out
+      local r, g, b, alpha = StarCursor.texture:GetVertexColor()
+      StarCursorSettings.r, StarCursorSettings.g, StarCursorSettings.b, StarCursorSettings.alpha = r, g, b, alpha
+  end
+end)
+
+
+-- Default Texture Values
+local x, y, speed = 0, 0, 0;
+local MAX_SPEED = 1024;
+local SPEED_DECAY = 2048;
+local SIZE_MODIFIER = 6;
+local MIN_SIZE = 16;
+
+local function isNan(value)
+  return value ~= value;
+end
+
+colorPickerOpen = false
 
 local function OnUpdate(_, elapsed)
-  --Somehow speed or something else is getting to -nan, reset if so (temporary fix by raymar_of_brovaria)
-  if speed + 1 == speed then
-    speed = 0
-  end
-  if x + 1 == x then
-    x = 0
-  end
-  if y + 1 == y then
-    y = 0
-  end
-  --end of temp fix
---local function OnUpdate(_, elapsed)
-  local dX = x;
-  local dY = y;
+  if isNan(speed) then speed = 0; end
+  if isNan(x) then x = 0; end
+  if isNan(y) then y = 0; end
+
+  local prevX, prevY = x, y;
   x, y = GetCursorPosition();
-  dX = x - dX;
-  dY = y - dY;
-  local weight = 2048 ^ -elapsed;
-  speed = math.min(weight * speed + (1 - weight) * math.sqrt(dX * dX + dY * dY) / elapsed, 1024);
-  local size = speed / 6 - 16;
-  if (size > 0) then
+  local dX, dY = x - prevX, y - prevY;
+
+  local distance = math.sqrt(dX * dX + dY * dY);
+  local decayFactor = SPEED_DECAY ^ -elapsed;
+  speed = math.min(decayFactor * speed + (1 - decayFactor) * distance / elapsed, MAX_SPEED);
+
+  local size = speed / SIZE_MODIFIER - MIN_SIZE;
+  if size > 0 then
     local scale = UIParent:GetEffectiveScale();
-    texture:SetHeight(size);
-    texture:SetWidth(size);
-    texture:SetPoint("CENTER", UIParent, "BOTTOMLEFT", (x + 0.5 * dX) / scale, (y + 0.5 * dY) / scale);
-    texture:Show();
+    StarCursor.texture:SetHeight(size);
+    StarCursor.texture:SetWidth(size);
+    StarCursor.texture:SetPoint("CENTER", UIParent, "BOTTOMLEFT", (x + dX) / scale, (y + dY) / scale);
+    StarCursor.texture:Show();
   else
-    texture:Hide();
+    StarCursor.texture:Hide();
   end
 end
+
 frame:SetScript("OnUpdate", OnUpdate);
